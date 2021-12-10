398bd3a7e0888c92a1aea40cd834b5b2897e5992
Beni <Beni@BENI-LAPTOP>
2021-12-10 19:03:50 +0100

    final commit

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public enum gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}

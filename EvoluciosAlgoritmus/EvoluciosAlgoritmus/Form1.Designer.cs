34fffed9dbe2eaff691cc77d0454d32b42bf4b0b
Beni <Beni@BENI-LAPTOP>
2021-12-29 15:50:16 +0100

    programmag

--- EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.Designer.cs
+++ EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.GenerationLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // GenerationLabel
+            // 
+            this.GenerationLabel.AutoSize = true;
+            this.GenerationLabel.Location = new System.Drawing.Point(317, 261);
+            this.GenerationLabel.Name = "GenerationLabel";
+            this.GenerationLabel.Size = new System.Drawing.Size(66, 13);
+            this.GenerationLabel.TabIndex = 0;
+            this.GenerationLabel.Text = "1. generáció";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 283);
+            this.Controls.Add(this.GenerationLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label GenerationLabel;
     }
 }
 
--- EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
+++ EvoluciosAlgoritmus/EvoluciosAlgoritmus/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace EvoluciosAlgoritmus
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            gc.GameOver += Gc_GameOver;
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+            
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            GenerationLabel.Text = string.Format("{0}. generáció", generation);
         }
     }
 }
